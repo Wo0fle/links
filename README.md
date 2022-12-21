f3cc1cc7d0d4b6e56e074c44de21014a86d8b405
Seby Amador <officiallywoofle@gmail.com>
2022-12-21 00:22:25 -0500

    almost done
    yay

--- README.md
+++
@@ -1,5 +0,0 @@
-# Links
-
-Source code for my website.
-
-> May or may not end up actually existing. This is really supposed to be an exercise for me really, to hone my web dev skills.
--- source/main.js
+++ source/main.js
@@ -1,34 +1,33 @@
-let curProfile = 'SEBY';
-const linkSections = document.getElementsByClassName("link-section")
-console.log(linkSections)
-// learn how to loop through HTMLCollectionOf
+const hideable = document.getElementsByClassName("hideable");
+let curProfile;
+let title;
 
 const changeProfile = () => {
-    if (curProfile === 'WOOFLE') {
-        curProfile = 'SEBY'
-        document.title = "Seby Amador's Links";
-
-        // unhide seby links
-        // hide woofle links
-
-        console.log("profile is now seby");
+    if (curProfile === 'woofle') {
+        curProfile = 'seby';
+        title = 'Seby Amador';
+    } else {
+        curProfile = 'woofle';
+        title = 'Woofle';
     }
-    else {
-        curProfile = 'WOOFLE'
-        document.title = "Woofle's Links";
 
-        // unhide woofle links
-        // hide seby links        
-
-        console.log("profile is now woofle");
+    for (let i = 0; i < hideable.length; i++) {
+        if (hideable[i].classList.contains(curProfile)) {
+            hideable[i].classList.remove('hidden');
+        }
+        else {
+            hideable[i].classList.add('hidden');
+        }
     }
+    document.title = title + "'s Links";
+    document.getElementById("name").innerHTML = title;
 }
 
 //////////////////////////////////////////////////////////
 
-changeProfile();
-
 window.onload = function() {
+    changeProfile();
+
     document.body.addEventListener('click', (event) => {
         if (event.target.tagName === 'IMG') {
             changeProfile();
