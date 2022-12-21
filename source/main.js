a57244e51cab541f85e9a7eab43c23920b7cbc77
Seby Amador <officiallywoofle@gmail.com>
2022-12-20 21:50:59 -0500

    gotta learn how to loop through HTMLCollectionOf

--- source/main.js
+++ source/main.js
@@ -1,5 +1,37 @@
-// switch from Woofle to Seby Amador
+let curProfile = 'SEBY';
+const linkSections = document.getElementsByClassName("link-section")
+console.log(linkSections)
+// learn how to loop through HTMLCollectionOf
 
-let changeProfile = () => {
-    console.log("change the profile")
+const changeProfile = () => {
+    if (curProfile === 'WOOFLE') {
+        curProfile = 'SEBY'
+        document.title = "Seby Amador's Links";
+
+        // unhide seby links
+        // hide woofle links
+
+        console.log("profile is now seby");
+    }
+    else {
+        curProfile = 'WOOFLE'
+        document.title = "Woofle's Links";
+
+        // unhide woofle links
+        // hide seby links        
+
+        console.log("profile is now woofle");
+    }
+}
+
+//////////////////////////////////////////////////////////
+
+changeProfile();
+
+window.onload = function() {
+    document.body.addEventListener('click', (event) => {
+        if (event.target.tagName === 'IMG') {
+            changeProfile();
+        } 
+    });
 }
