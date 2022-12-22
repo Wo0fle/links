ec457d7108fb3e989bebebdf988699771f04e1f2
Seby Amador <officiallywoofle@gmail.com>
2022-12-21 23:08:58 -0500

    last lil bit of beauty
    v1 done?

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 const hideable = document.getElementsByClassName("hideable");
 let curProfile;
-let title = "Seby Amador";
+let title;
 
 const changeProfile = () => {
-    document.getElementById("name-location-2").innerHTML = title;
     if (curProfile === 'woofle') {
         curProfile = 'seby';
         title = 'Seby Amador';
+
+        document.querySelector("link[rel*='icon']").href = "./images/seby.jpg";
+
+        document.getElementById("other-profile-message").innerHTML = 'Woofle-related links';
     } else {
         curProfile = 'woofle';
         title = 'Woofle';
+
+        document.querySelector("link[rel*='icon']").href = "./images/woofle.jpg";
+
+        document.getElementById("other-profile-message").innerHTML = 'music-related links';
     }
 
     for (let i = 0; i < hideable.length; i++) {
@@ -21,7 +28,7 @@ const changeProfile = () => {
         }
     }
     document.title = title + "'s Links";
-    document.getElementById("name-location-1").innerHTML = title;
+    document.getElementById("name-location").innerHTML = title;
 }
 
 //////////////////////////////////////////////////////////
